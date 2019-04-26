61bc4104a7ba4a6e1b78f24d89b30ea3828f3b19
Oleg Moskalenko <oleg.moskalenko@gmail.com>
2019-04-26 11:55:50 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(function () {
+  var mainNav = document.querySelector('.main-nav');
+  var toggle = document.querySelector('.main-nav__toggle');
+
+  toggle.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (mainNav.classList.contains('main-nav--open')) {
+      mainNav.classList.remove('main-nav--open');
+    } else {
+      mainNav.classList.add('main-nav--open')
+    }
+  });
+})();
+
+(function () {
+  var mainNav = document.querySelector('.main-nav');
+  var toggle = document.querySelector('.main-nav__toggle');
+
+  toggle.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (mainNav.classList.contains('main-nav--open')) {
+      mainNav.classList.remove('main-nav--open');
+    } else {
+      mainNav.classList.add('main-nav--open')
+    }
+  });
+})();
