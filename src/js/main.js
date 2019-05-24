5d1e9d2ae389e44dba72dc8e4879980b4afd2122
Oleg Moskalenko <oleg.moskalenko@gmail.com>
2019-05-24 16:27:58 +0300

    JS for slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
+/* ===============================
+            SLIDER
+================================== */
 (function () {
   var slider = document.querySelector(".feedbacks__wrapper");
   var slides = slider.getElementsByClassName("feedbacks__item");
+  var controls = document.getElementById("feedbacks__controls");
   var dots = document.querySelectorAll(".feedbacks__dot");
   var slideIndex = 1;
   var size = slider.clientWidth;
+
+  showSlides(slideIndex);
 
   // Thumbnail image controls
   function currentSlide(n) {
@@ -14,20 +20,27 @@
     if (n > slides.length) slideIndex = 1;
     if (n < 1) slideIndex = slides.length;
 
+    slider.style.width = slides.length * 100 + "%";
+
+    for (var index = 0; index < slides.length; index++) {
+      slides[index].style.width = 100 / slides.length + "%";
+    }
+
+    // убираем класс active у элементов управления dots
     for (var i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    slider.style.transform = "translateX(" + (-(slideIndex - 1) * size) + "px";
-    dots[slideIndex - 1].className += " active";
+    //перемещаем слайдер на нужное количество px
+    slider.style.marginLeft = -(100 * (slideIndex-1)) + "%";
+    //добавляем класс active точке на которую нажали
+    dots[slideIndex-1].className += " active";
   }
 
-  showSlides(slideIndex);
-  for (var i = 0; i < dots.length; i++) {
-    alert(dots[i] );
-    // dots[i].addEventListener("click", function () {
-    //    currentSlide(i+1);
-    //   alert(i+1);
-    // });
-  }
+  // событие клика на блоке с точками
+  controls.addEventListener("click", function(event) {
+    var target = event.target; // элемент на котором кликнули
+    var num = target.getAttribute('data-item'); // получаем атрибут data-item элемента, на котором кликнули
+    if(num) currentSlide(num);
+  });
 })();
